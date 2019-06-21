e9bb1e5029100f79e50ef5620a57a906b946ef35
Kate Donaldson <kate@katelovescode.com>
2019-06-21 15:24:02 -0500

    Adds the loadpath and expands the say method

--- lib/aphorism.rb
+++ lib/aphorism.rb
@@ -1,10 +1,14 @@
 require "aphorism/version"
+require "pry"
 
 module Aphorism
   class Error < StandardError; end
   class Orator
-    def say_hello
-      puts "Hello World!"
+    def say
+      aphorism_path = File.join(__dir__, 'source/quotes.txt')
+      aphorism = File.open(aphorism_path)
+      binding.pry
+      puts aphorism
     end
   end
 end
