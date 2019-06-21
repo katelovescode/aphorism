587ad4e45134c0555b8a5d35685a8d4c5834735b
Kate Donaldson <kate@katelovescode.com>
2019-06-21 16:31:07 -0500

    Corrects the say method to use the new aphorisms path

--- lib/aphorism.rb
+++ lib/aphorism.rb
@@ -5,7 +5,7 @@ module Aphorism
   class Error < StandardError; end
   class Orator
     def say
-      aphorisms_path = File.join(__dir__, 'source/quotes.txt')
+      aphorisms_path = File.join(__dir__, 'content/aphorisms.txt')
       aphorisms = IO.read(aphorisms_path).split("\n%\n")
       puts aphorisms.sample
     end
