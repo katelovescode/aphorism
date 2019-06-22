47baaf381047ad7135e71c8dd8ede5922e94e16d
Kate Donaldson <kate@katelovescode.com>
2019-06-21 22:51:20 -0500

    Replaces Dir.children with Dir.entries and subtracts the pointers

--- lib/aphorism.rb
+++ lib/aphorism.rb
@@ -21,7 +21,7 @@ module Aphorism
 
     def custom_aphorisms
       custom_path = File.join(Dir.home, '.aphorism')
-      custom_files = Dir.children(custom_path)
+      custom_files = Dir.entries(custom_path) - [".", ".."]
       custom_aphorisms = []
       custom_files.each { |file| custom_aphorisms += IO.read(File.join(custom_path, file)).split("\n%\n") }
       custom_aphorisms
