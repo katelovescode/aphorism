5976a48c8e319b2faddbdd38edb5131ca3da750a
Kate Donaldson <kate@katelovescode.com>
2019-06-21 22:20:11 -0500

    Adds functionality for a custom ~/.aphorism path for custom quotes

--- lib/aphorism.rb
+++ lib/aphorism.rb
@@ -1,12 +1,27 @@
 require "aphorism/version"
+require "pry"
 
 module Aphorism
   class Error < StandardError; end
   class Orator
     def say
+      aphorisms = internal_aphorisms + custom_aphorisms
+      puts (aphorisms).sample
+    end
+
+    private
+    
+    def internal_aphorisms
       aphorisms_path = File.join(__dir__, 'content/aphorisms.txt')
-      aphorisms = IO.read(aphorisms_path).split("\n%\n")
-      puts aphorisms.sample
+      IO.read(aphorisms_path).split("\n%\n")
+    end
+
+    def custom_aphorisms
+      custom_path = File.join(Dir.home, '.aphorism')
+      custom_files = Dir.children(custom_path)
+      custom_aphorisms = []
+      custom_files.each { |file| custom_aphorisms += IO.read(File.join(custom_path, file)).split("\n%\n") }
+      custom_aphorisms
     end
   end
 end
