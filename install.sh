54d9f23b02920a10924c4f2cd45093a755c2a94f
Kate Donaldson <kate@katelovescode.com>
2020-04-15 19:48:30 -0500

    Adds Install script, and updates some of the quotes

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if which ruby >/dev/null 2>&1; then
+  if which rvm >/dev/null 2>&1; then
+    source ~/.rvm/scripts/rvm
+    rvm use default >/dev/null 2>&1
+  fi
+  if [ `gem list '^aphorism$' -i` = true ] && [ `gem list '^lolcat$' -i` = true ]; then
+    aphorism | lolcat
+  else
+    echo "initializing aphorism with lolcat"
+    gem install bundler aphorism lolcat --silent
+    aphorism | lolcat
+  fi
+else
+  echo "Ruby is required for aphorism to work, please install ruby and try again"
+fi
